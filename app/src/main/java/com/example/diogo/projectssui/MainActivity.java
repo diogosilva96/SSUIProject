feabc1300fe22b3a59f227acf4da343d125a6185
diogosilva96 <diogo_silva_96@hotmail.com>
2018-04-30 14:04:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/diogo/projectssui/MainActivity.java
@@ -0,0 +1,149 @@
+package com.example.diogo.projectssui;
+
+import android.app.Activity;
+import android.graphics.PixelFormat;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends Activity  {
+
+    private SurfaceView surfaceView;
+    private Button btnPlay;
+    private SeekBar timeLine;
+    private MediaPlayer mediaPlayer = null;
+    private TextView currentTime;
+    private TextView totalDuration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        timeLine.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    int skipTo = timeLine.getProgress();
+                    mediaPlayer.seekTo(skipTo);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.pause();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.start();
+
+            }
+        });
+
+        MainActivity.this.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                monitorHandler.postDelayed(this, 500);
+                monitorHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mediaPlayerMonitor();
+                    }
+                });
+            }
+        });
+
+
+    }
+
+    private Handler monitorHandler = new Handler();
+
+
+
+
+    private void init() {
+        // TODO Auto-generated method stub
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView1);
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        timeLine = (SeekBar) findViewById(R.id.durationBar);
+        timeLine.setVisibility(View.INVISIBLE);
+        currentTime = (TextView) findViewById(R.id.currentTime);
+        totalDuration = (TextView) findViewById(R.id.totalDuration);
+    }
+
+
+
+    public void PlayStop(View v){
+        try{
+            if(mediaPlayer != null){
+                if(mediaPlayer.isPlaying()) {
+                    btnPlay.setText("Play");
+                    mediaPlayer.pause();
+                    Toast.makeText(getApplicationContext(), "Pausing!", Toast.LENGTH_LONG).show();
+                } else {
+                    btnPlay.setText("Pause");
+                    mediaPlayer.start();
+                    Toast.makeText(getApplicationContext(), "Playing!", Toast.LENGTH_LONG).show();
+                }
+            }
+            else{
+                initVideoPlayer();
+                mediaPlayer.start();
+                btnPlay.setText("Pause");
+
+                Toast.makeText(getApplicationContext(), "Started video play", Toast.LENGTH_LONG).show();
+            }
+
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void initVideoPlayer(){
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+        mediaPlayer = MediaPlayer.create(this, R.raw.videoplayback);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        timeLine.setVisibility(View.VISIBLE);
+        //surfaceHolder.setFixedSize(176, 144);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        mediaPlayer.setDisplay(surfaceHolder);
+        int mediaDuration = mediaPlayer.getDuration();
+        timeLine.setMax(mediaDuration);
+        String TotalDuration = CalculateTime(mediaDuration);
+        totalDuration.setText(TotalDuration);
+
+    }
+
+    private void mediaPlayerMonitor(){ //monitor que é chamado par atualizar a view
+        if (mediaPlayer != null){
+            //int mediaDuration = mediaPlayer.getDuration();
+            int mediaPosition = mediaPlayer.getCurrentPosition();
+            //timeLine.setMax(mediaDuration);
+            timeLine.setProgress(mediaPosition);
+            String CurrentTime = CalculateTime(mediaPosition);
+            currentTime.setText(CurrentTime);
+        }
+    }
+
+    private String CalculateTime(int Time){// input is time in milliseconds
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(Time); //converts to seconds
+        String time =secondsToString(seconds);
+        return time;
+    }
+
+    private String secondsToString(long timeInSeconds){
+        return String.format("%02d:%02d:%02d",timeInSeconds/(60*60),timeInSeconds /60, timeInSeconds %60);// formata o tempo para uma string com(hour:minute:seconds)
+    }
+}
